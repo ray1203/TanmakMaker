d7d4b10f8554163b6d71cf1eb584b3fbd8ce4bf1
정재현 <ray1203@naver.com>
2019-09-13 17:52:30 +0900

    도윰말 추가(sharedasset9 제외하고 올림)

---
+++ BIN/Assets/Script/Tutorial.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject Images;
+    public int page;
+    private int pageMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pageMax = Images.transform.childCount;
+        for(int i = 0; i < pageMax; i++) {
+            Images.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        Images.transform.GetChild(0).gameObject.SetActive(true);
+        page = 0;
+    }
+
+    public void next() {
+        if (page+1 < pageMax) {
+            Images.transform.GetChild(page).gameObject.SetActive(false);
+            page += 1;
+            Images.transform.GetChild(page).gameObject.SetActive(true);
+        }
+    }
+    public void back() {
+        if (page - 1 >= 0) {
+            Images.transform.GetChild(page).gameObject.SetActive(false);
+            page -= 1;
+            Images.transform.GetChild(page).gameObject.SetActive(true);
+        }
+    }
+}
